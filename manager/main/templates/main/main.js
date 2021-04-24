5241520657a085cba4ddcec32035b5ac4f67bf26
Лавриненко Алексей <a.lavrinenko@brandquad.ru>
2021-04-25 00:47:44 +0300

    add templates

---
+++ manager/main/templates/main/main.js
@@ -0,0 +1,50 @@
+// AJAX
+function getGroups() {
+    // Functions returns list of groups 
+
+}
+
+function addGroup() {
+    // Functions to create a new group
+}
+
+function delGroup() {
+    // function to del group by name 
+
+}
+
+function getAccounts() {
+    // 
+
+}
+
+function addAccount() {
+    // Functions make a request to get all accounts of some group
+
+}
+
+function getAccount() {
+    // Functions make a request to get all accounts of some group
+
+}
+
+function delAccount() {
+    // Functions make a request to get all accounts of some group
+
+}
+
+function updateAccount() {
+    // Functions make a request to get all accounts of some group
+
+}
+
+function setMasterKey() {
+    // Functions make a request to get all accounts of some group
+
+}
+
+function checkMasterKey() {
+    // Functions make a request to get all accounts of some group
+
+}
+
